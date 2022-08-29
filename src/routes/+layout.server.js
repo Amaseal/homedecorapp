1da7c2b3bc1841ebaeb6d8c10db89586a1e5179e
arturs jersovs <freelance.jershov@gmail.com>
2022-08-29 16:08:48 +0300

    test

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,6 @@
 export async function load() {
   console.log("layout");
-  const res = await fetch(
-    "https://cms.sghomedecor.lv/api/categories?populate=image"
-  );
+  const res = await fetch("localhost:1337/api/categories?populate=image");
   const categories = await res.json();
   return {
     data: categories.data,
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 export async function load() {
   console.log("index");
   const res = await fetch(
-    "https://cms.sghomedecor.lv/api/home?populate=background,quotes,about,about.image"
+    "localhost:1337/api/home?populate=background,quotes,about,about.image"
   );
   const home = await res.json();
   return {
